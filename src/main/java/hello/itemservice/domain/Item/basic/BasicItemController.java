8fb9a46acf1dd887f3b2b977ff1225b451770981
devpigKing <devpigking@gmail.com>
2023-07-27 22:45:55 +0900

    #feat : 상품 수정 처리에 대한 뷰 템플릿과 컨트롤러에 대한 코드입니다.

--- src/main/java/hello/itemservice/domain/Item/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/domain/Item/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -57,13 +58,26 @@ public class BasicItemController {
 
     // @ModelAttribute는 Item 객체를 생성하고, 요청 파라미터의 값을 프로퍼티 접근법(setXxx)으로
     // 입력해준다.
-    @PostMapping("/add")
-    public String addItemV2(@ModelAttribute Item item) {
-        itemRepository.save(item);
-        // model.addAttribute("item", item); // 자동 추가, 생략 가능
-        return "basic/item";
-    }
+//    @PostMapping("/add")
+//    public String addItemV2(@ModelAttribute Item item) {
+//        itemRepository.save(item);
+//        // model.addAttribute("item", item); // 자동 추가, 생략 가능
+//        return "basic/item";
+//    }
 
+//    @PostMapping("/add")
+//    public String addItemV5(Item item) {
+//        itemRepository.save(item);
+//        return "redirect:/basic/items/" + item.getId();
+//    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
+    }
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
@@ -71,7 +85,11 @@ public class BasicItemController {
         return "basic/editForm";
     }
 
-
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
 
     /*
     테스트용 데이터 추가
